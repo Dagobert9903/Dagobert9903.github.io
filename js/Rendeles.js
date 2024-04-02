1475ab8506864783afb0069a83c7393bfc1abcfc
Tóth Péter <tothpetyka3002@gmail.com>
2024-04-02 13:44:12 +0200

    Init page.

--- js/Rendeles.js
+++ js/Rendeles.js
@@ -1,10 +1,8 @@
 function SendOrderData() {
-    let price = 1200;
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.message");
-    
-    let amount = parseInt(amountInput.value) * price;
-    
-
-    message.innerHTML = amount;
+    const message = document.querySelector('#message');
+    const extra = parseInt(document.querySelector('[name=extra]:checked').value, 10);
+    const sauce = parseInt(document.querySelector('#sauce').value, 10);
+    const quantity = parseInt(document.querySelector('#quantity').value, 10);
+    const price = (1200 + extra + sauce) * quantity;
+    message.innerText = price;
 }
