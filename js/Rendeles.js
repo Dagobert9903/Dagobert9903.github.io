4044ee312471d8f5ed6aed34cc3ee944d8a1ac3f
Tóth Péter <tothpetyka3002@gmail.com>
2024-04-05 11:41:21 +0200

    Init page.

--- js/Rendeles.js
+++ js/Rendeles.js
@@ -1,4 +1,5 @@
-function sendOrderData(){
+function sendOrderData() {
+
     const name = document.querySelector('#name').value.trim();
     const email = document.querySelector('#email').value.trim();
     const address = document.querySelector('#address').value.trim();
@@ -8,19 +9,48 @@ function sendOrderData(){
     const sauce = parseInt(document.querySelector('#sauce').value, 10);
     const quantity = parseInt(document.querySelector('#quantity').value, 10);
 
+    const isPersonalDataValid = isValidPersonalData(name, email, address, comment);
+    const isQuantityValid = isValidQuantity(quantity);
+    const totalPrice = price(extra, sauce, quantity);
+
+    if (isPersonalDataValid && isQuantityValid) {
+        message.innerText = totalPrice;
+    }
+}
+
+function isValidPersonalData(name, email, address, comment) {
+
     if (!name) {
         alert('A név megadása kötelező!');
-    } else if (!email || !(email.indexOf('@') > 0) || !(email.indexOf('.') > 0)) {
-        alert('Invalid email cím!');
-    } else if (address.length < 10) {
-        alert('Kérjük pontos címet adj meg!');
-    } else if (comment.indexOf('<') > 0 && comment.indexOf('>') > 0) {
-        alert('A megjegyzésben nem megengedett HTML használata!');
-    } else if (!quantity || quantity < 1 || quantity > 10) {
-        alert('Csak 1 és 10 közötti darabot rendelhetsz!');
-    } else {
-        const message = document.querySelector('#message');
-        const price = (1200 + extra + sauce) * quantity;
-        message.innerText = price;
+        return false;
     }
+    if (!email || !(email.indexOf('@') > 0) || !(email.indexOf('.') > 0)) {
+        alert('Invalid email cím!');
+        return false;
+    }
+    if (address.length < 10) {
+        alert('Kérjük pontos címet adj meg!');
+        return false;
+    }
+    if (comment.indexOf('<') > 0 && comment.indexOf('>') > 0) {
+        alert('A megjegyzésben nem megengedett HTML használata!');
+        return false;
+    }
+    return true;
+}
+
+function isValidQuantity(quantity) {
+    if (!quantity || quantity < 1 || quantity > 10) {
+        alert('Csak 1 és 10 közötti darabot rendelhetsz!');
+        return false;
+    }
+    return true;
+}
+
+function price(extra, sauce, quantity) {
+    let price = (1200 + extra + sauce) * quantity;
+    if (price < 5000) {
+        price += 500;
+    }
+    return price;
 }
